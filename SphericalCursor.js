4bfecd916880d376c20f52a63d545f21aead7a11
AndrewBresee <andrew.p.bresee@gmail.com>
2016-05-12 20:29:14 -0700

    Clean up code and remove comments

--- SphericalCursor.js
+++ SphericalCursor.js
@@ -27,12 +27,13 @@ var SphericalCursor = function() {
 	function update() {
 	
 		// this will serve as the new "origin" for the cursor
+		// where the user is standing
 		// the cursor will then move around this point
 		var currentPosition = controls.getObject().position;
+
 		var xLocation = currentPosition.x; 
 		var yLocation = currentPosition.y; 
 		var zLocation = currentPosition.z;
-
 
 		// this is the vector where the camera is looking
 		var pointerVector = camera.getWorldDirection();
@@ -41,37 +42,28 @@ var SphericalCursor = function() {
 		var vectorY = pointerVector.y;
 		var vectorZ = pointerVector.z;
 
-		// make a ray that extends out from the user to the max distance
-		// if the line intersects with anything, make the cursor at that location
-		// + a little 
-		// make the line invisable 
-
-		// the cursor will be at a fixed location close to the user
-		// unless there is an object a certain distance away
-		// if that object is within the max distance, then the cursor will go to that
-
-
 		// http://threejs.org/docs/#Reference/Core/Raycaster.intersectObject
+		// creates a ray (vector) in which the user is looking
 		var raycaster = new THREE.Raycaster();
 		mouse = new THREE.Vector2();
 		raycaster.setFromCamera( mouse, camera );	
 
-		//Checks to see if user is interacting with a box
+		// checks to see if cursor is interacting with a box
 		var intersects = raycaster.intersectObjects( objects );
 		var intersectsFloor = raycaster.intersectObjects( floorObject );
 
 		targetItem = 0; 
 		var minItemDistance = 100;
 
-
-
 		if (intersects.length >= 1 ) {
+
+			// checks if the cube is within the maximum distance
 			if (intersects[0].distance > targetItem && intersects[0].distance < MAX_DISTANCE) {
 
 					// code attempting to update color when mouse is over one cube face
-					// intersects[0].face.color.setRGB( 0.8 * Math.random() + 0.2, 0, 0 ); 
-					// // this allows the target cube color face to be updated
-					// intersects[0].object.geometry.colorsNeedUpdate = true;
+						// intersects[0].face.color.setRGB( 0.8 * Math.random() + 0.2, 0, 0 ); 
+						// // this allows the target cube color face to be updated
+						// intersects[0].object.geometry.colorsNeedUpdate = true;
 
 					targetItem = intersects[0];
 
@@ -86,7 +78,6 @@ var SphericalCursor = function() {
 					cursor.position.x = itemLocation.x;
 					cursor.position.y = itemLocation.y;
 					cursor.position.z = itemLocation.z;
-
 					
 					// place the cursor on the cube face
 					// with slight difference for cube center
@@ -113,6 +104,7 @@ var SphericalCursor = function() {
 						}
 					}
 				} 
+
 		} else {
 
 			// sets the cursor to be at a certain distance 
@@ -121,17 +113,6 @@ var SphericalCursor = function() {
 			cursor.position.y = yLocation + (vectorY * 50);
 			cursor.position.z = zLocation + (vectorZ * 50);
 		}
-
-
-		
-		// check to see if max or min are objects 
-		// if minItemDistance is an object, then put the cursor there
-		// else if targetItem is an object, put the cursor there
-		// otherwise, put the cursor at its fixed distance.  
-
-		// if the targetItem is an object
-		// change it back to its original color; 
-
 	}
 
 	function onMouseMove( event ) {
